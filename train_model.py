61290566299046d6adb89ce193cba1eaac199d51
Joe Kerrigan <joek1301@gmail.com>
2022-12-08 23:23:30 -0500

    Add data augmentation; support resume training from ckpt

--- train_model.py
+++ train_model.py
@@ -10,26 +10,40 @@ from torchvision import datasets, transforms
 import mia
 
 BATCH_SIZE = 32
-EPOCHS = 10
+EPOCHS = 50
+
+START_FROM = 0
 
 if __name__ != "__main__":
     exit(0)
 
 # use gpu if available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
-model = mia.create_model().to(device)
+
+if START_FROM == 0:
+    model = mia.create_model().to(device)
+else:
+    model = mia.load_model(f"./models/resnet_epoch{START_FROM}.pth").to(device)
 
 # dataset transforms
-transform = transforms.Compose([transforms.ToTensor(), 
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
 # use CIFAR10 training/test set
-trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
                                           shuffle=True, num_workers=0)
 
-testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
                                          shuffle=False, num_workers=0)
 
@@ -53,7 +67,7 @@ def print_statistics(epoch, train_loss):
 
     print(f"[epoch {epoch + 1}] train loss: {train_loss:.3f} test loss: {test_loss / n_test:.3f} test accuracy: {test_correct / n_test:.3f}")
 
-for epoch in range(EPOCHS):
+for epoch in range(START_FROM, START_FROM + EPOCHS):
     running_loss = 0.0
     instances = 0
     
